7c717d36d906fc8e21cc378de63f3b50a4d7848a
Muhammad Aazmeer <aazmeer.official@gmail.com>
2026-02-24 03:27:01 +0500

    Update MongoDB connection and add chat example
    Refactor MongoDB connection and add chat model instantiation.

--- mongo_boilerplate.js
+++ mongo_boilerplate.js
@@ -6,6 +6,7 @@ const path = require("path");
 const methodOverride = require('method-override');
 const { faker } = require('@faker-js/faker');
 const mongoose = require('mongoose');
+const Chat = require("./models/chat.js")
 
 // EXPRESS REQUIREMENTS
 
@@ -17,8 +18,24 @@ app.use(methodOverride('_method'))
 
 // DATABASE REQUIREMENTS
 
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+main()
+.then(()=>{console.log("connection Successful")})
+.catch(err => console.log(err));
 
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+let chat1 = new Chat({
+    from:"Aazmeer",
+    to: "ruhab",
+    message:"Send me the notes",
+    created_at: new Date()
+})
+
+chat1.save().then((res)=>{
+    console.log(res)
+})
 // Express App
 
 app.get("/",(req,res)=>{
